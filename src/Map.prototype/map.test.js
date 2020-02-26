40005bc97bfcb178b69c0e0445aeb3f76bdd7ee6
xiaomeiwu <xiaomeiwu2020@outlook.com>
2020-02-26 09:53:58 +0800

    feat: Map

--- src/Map.prototype/map.test.js
+++ src/Map.prototype/map.test.js
@@ -1,11 +1,28 @@
-let param = [
-  [0, 0],
-  [1, 1],
-  [2, 2]
-];
-let { toString } = Object.prototype;
-let i = new Map(param);
-test("", function() {
-  expect(i).toEqual(new Map(param));
-  console.log(typeof i, toString.call(i));
-});
+const a = [
+  ['x', 'this is x'],
+  ['y', 'this is y']
+]
+const b = new Map(a)
+const keys = [
+  'constructor',
+  'get',
+  'set',
+  'has',
+  'delete',
+  'clear',
+  'entries',
+  'forEach',
+  'keys',
+  'size',
+  'values'
+]
+test('', function() {
+  expect(Object.keys(Object.getOwnPropertyDescriptors(Map.prototype))).toEqual(
+    keys
+  )
+  expect(b.has('x') && b.has('y')).toBe(true)
+  expect([b.get('x'), b.get('y')]).toEqual(['this is x', 'this is y'])
+  expect(b.size).toBe(2)
+  b.clear()
+  expect(b.size).toBe(0)
+})
